d52cece92b74308937792c5403060a34ad0e9476
Astitva Srivastava <srivastaway@gmail.com>
2025-10-19 11:37:05 +0530

    commit on moshi branch

--- src/components/NewStuff.jsx
+++ src/components/NewStuff.jsx
@@ -4,6 +4,7 @@ const NewStuff = () => {
   return (
     <div>
         <h1>what will you do with this new change?</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur nam doloribus provident soluta dolores aspernatur quam, qui delectus odit rem eius itaque eum quia earum molestiae? Vitae dolor quod ad dolorem minus, laboriosam aliquam, cupiditate at illum veritatis consequatur explicabo atque qui quae? Ipsa minima laboriosam libero deleniti commodi beatae.</p>
     </div>
   )
 }
